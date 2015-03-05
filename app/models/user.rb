1c3aed7ed525022cfb51567a12c05aa879727a82
Deb Wolfe & Zac <pair+debwolfe.zbarnes757@devbootcamp.com>
2015-03-05 12:33:40 -0800

    create user model with bcrypt methods and helpers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  # users.password_hash in the database is a :string
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
