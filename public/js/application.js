79c48c2023c63d674ffcba3d071eda9511f6666b
jessiewuwu & Catie Kennedy <pair+jessiewuwu.yaysunshine@devbootcamp.com>
2015-03-06 12:34:03 -0800

    alternating turns for red and black

--- public/js/application.js
+++ public/js/application.js
@@ -1,30 +1,66 @@
-$(document).ready(function() {
-  $('td').click(function(event) {
-    var column = $(this).attr('class').split(' ')[0];
-    var row = $(this).parent().attr('id');
-    // console.log(column+ ' ' + row);
-    var lastSlot = $('.' + column + '.unfilled').last();
-    lastSlot.removeClass('unfilled').addClass('filled');
-    lastSlot.find('circle').attr('fill', 'red');
 
+Game.redTurn = true;
+
+Game.bindEvents = function(){
+  $('td').click(function(event){
+    event.preventDefault();
+    Game.handleClick($(this))
   });
+};
+
+Game.handleClick = function(td) {
+  var color = this.redTurn ? 'red' : 'black'
+  var column = td.attr('class').split(' ')[0];
+  var row = td.parent().attr('id');
+  // console.log(column+ ' ' + row);
+  var lastSlot = $('.' + column + '.unfilled').last();
+  lastSlot.removeClass('unfilled').addClass('filled');
+  lastSlot.find('circle').attr('fill', color);
+  this.redTurn = !this.redTurn
+};
+
+
+$(document).ready(function() {
+
+  Game.bindEvents();
+
 });
+
+
+
+
+
+    // redTurn === !redTurn if (redTurn ? 'red' : 'black');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // columnCheck(colNum, playerColor)
 
-function columnOneCheck(){
-  var row6Column1 = $('#r6-c1 circle')
-  $('.c1').on('click', function(){
-    row6Column1.attr("fill", "red");
-  })
-}
+// function columnOneCheck(){
+//   var row6Column1 = $('#r6-c1 circle')
+//   $('.c1').on('click', function(){
+//     row6Column1.attr("fill", "red");
+//   })
+// }
 
-function columnTwoCheck(){
-  var row6Column2 = $('#r6-c2 circle')
-  $('.c2').on('click', function(){
-    row6Column2.attr("fill", "red");
-  })
-}
+// function columnTwoCheck(){
+//   var row6Column2 = $('#r6-c2 circle')
+//   $('.c2').on('click', function(){
+//     row6Column2.attr("fill", "red");
+//   })
+// }
 
 
 
